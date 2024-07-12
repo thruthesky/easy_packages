cfdf094a1f5d8d06bca989990db74477b38ca9a3
Noe <withcenter.dev4@gmail.com>
2024-07-12 11:08:27 +0800

    comented out unused code import

--- easychat/lib/src/chat.room.dart
+++ easychat/lib/src/chat.room.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_database/firebase_database.dart';
+// import 'package:firebase_database/firebase_database.dart';
 
 class ChatRoom {
   CollectionReference get col =>
