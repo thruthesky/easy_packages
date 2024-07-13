e3a13a6ec14f573118073216eee00887ba64a69c
JaeHo Song <thruthesky@gmail.com>
2024-07-13 10:20:05 +0800

    doc

--- easyuser/lib/src/user.defines.dart
+++ easyuser/lib/src/user.defines.dart
@@ -2,6 +2,8 @@ import 'package:easyuser/easyuser.dart';
 
 /// [iam] and [i] are the simple aliases of UserService.instance
 UserService get iam => UserService.instance;
+
+/// [iam] and [i] are the simple aliases of UserService.instance
 UserService get i => iam;
 
 /// [my] is the simple alias of [iam.user]
