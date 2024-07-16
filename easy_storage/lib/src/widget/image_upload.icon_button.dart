a289d97aa7243953b5a4c7c51080f738dae3aa60
Charles <withcenter.dev3@gmail.com>
2024-07-16 17:33:23 +0800

    update image_upload.icon button

--- easy_storage/lib/src/widget/image_upload.icon_button.dart
+++ easy_storage/lib/src/widget/image_upload.icon_button.dart
@@ -8,19 +8,41 @@ class ImageUploadIconButton extends StatelessWidget {
   const ImageUploadIconButton({
     super.key,
     required this.onUpload,
+    this.camera = true,
+    this.gallery = true,
+    this.progress,
+    this.complete,
     this.icon,
+    this.iconSize,
+    this.visualDensity,
+    this.padding,
   });
 
   final void Function(String url) onUpload;
   final Widget? icon;
+  final Function(double)? progress;
+  final Function()? complete;
+  final VisualDensity? visualDensity;
+  final bool camera;
+  final bool gallery;
+
+  final double? iconSize;
+  final EdgeInsetsGeometry? padding;
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
       icon: icon ?? const Icon(Icons.camera_alt),
+      iconSize: iconSize,
+      visualDensity: visualDensity,
+      padding: padding,
       onPressed: () async {
-        String? uploadedUrl = await StorageService.instance.upload(
+        final uploadedUrl = await StorageService.instance.upload(
           context: context,
+          camera: camera,
+          gallery: gallery,
+          progress: progress,
+          complete: complete,
         );
         if (uploadedUrl != null) {
           onUpload.call(uploadedUrl);
