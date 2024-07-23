765c53a869d6855edfa25acba40ce973e08ea94d
Noe <withcenter.dev4@gmail.com>
2024-07-23 09:34:02 +0800

    update on youtube_screen

--- easy_post/lib/src/screens/youtube/youtube.screen.dart
+++ easy_post/lib/src/screens/youtube/youtube.screen.dart
@@ -1,3 +1,4 @@
+import 'package:easy_locale/easy_locale.dart';
 import 'package:easy_post_v2/easy_post_v2.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart' as ypf;
@@ -46,145 +47,172 @@ class _YoutubeScreenState extends State<YoutubeScreen> {
   @override
   Widget build(BuildContext context) {
     return ypf.YoutubePlayerBuilder(
-        player: ypf.YoutubePlayer(
-          controller: youtubeController,
-          topActions: const [],
-          bottomActions: [
+      player: ypf.YoutubePlayer(
+        controller: youtubeController,
+        topActions: const [],
+        bottomActions: [
+          IconButton(
+              onPressed: () {
+                youtubeController.value.isPlaying
+                    ? youtubeController.pause()
+                    : youtubeController.play();
+              },
+              icon: Icon(
+                youtubeController.value.isPlaying
+                    ? Icons.pause
+                    : Icons.play_arrow,
+                color: Colors.white,
+              )),
+          ypf.CurrentPosition(),
+          ypf.ProgressBar(
+            colors: const ypf.ProgressBarColors(
+                playedColor: Colors.white,
+                handleColor: Colors.white,
+                backgroundColor: Colors.grey),
+            isExpanded: true,
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          ypf.RemainingDuration(),
+          ypf.FullScreenButton(),
+        ],
+      ),
+      builder: (context, player) => Scaffold(
+        appBar: AppBar(
+          title: Text('YouTube'.t),
+          actions: [
             IconButton(
-                onPressed: () {
-                  youtubeController.value.isPlaying
-                      ? youtubeController.pause()
-                      : youtubeController.play();
-                },
-                icon: Icon(
-                  youtubeController.value.isPlaying
-                      ? Icons.pause
-                      : Icons.play_arrow,
-                  color: Colors.white,
-                )),
-            ypf.CurrentPosition(),
-            ypf.ProgressBar(
-              colors: const ypf.ProgressBarColors(
-                  playedColor: Colors.white,
-                  handleColor: Colors.white,
-                  backgroundColor: Colors.grey),
-              isExpanded: true,
+              icon: const Icon(Icons.add),
+              onPressed: () async {
+                await PostService.instance.showPostEditScreen(
+                  context: context,
+                  category: 'youtube',
+                );
+              },
             ),
-            const SizedBox(
-              width: 8,
-            ),
-            ypf.RemainingDuration(),
-            ypf.FullScreenButton(),
           ],
         ),
-        builder: (context, player) => Scaffold(
-            appBar: AppBar(
-              title: const Text('YouTube'),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: () async {
-                    await PostService.instance.showPostEditScreen(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // separate the youtube player so it can be reused
+              ypf.YoutubePlayer(
+                  controller: youtubeController,
+                  topActions: const [],
+                  bottomActions: [
+                    IconButton(
+                        onPressed: () {
+                          youtubeController.value.isPlaying
+                              ? youtubeController.pause()
+                              : youtubeController.play();
+                        },
+                        icon: Icon(
+                          youtubeController.value.isPlaying
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                          color: Colors.white,
+                        )),
+                    ypf.CurrentPosition(),
+                    ypf.ProgressBar(
+                      colors: const ypf.ProgressBarColors(
+                          playedColor: Colors.white,
+                          handleColor: Colors.white,
+                          backgroundColor: Colors.grey),
+                      isExpanded: true,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    ypf.RemainingDuration(),
+                    ypf.FullScreenButton(),
+                  ]),
+              const SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  '${widget.post.youtube['title']}',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  widget.post.title,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  widget.post.content,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextButton(
+                  onPressed: () {
+                    youtubeController.pause();
+                    PostService.instance.showPostDetailScreen(
                       context: context,
-                      category: 'youtube',
+                      post: widget.post,
                     );
                   },
-                ),
-              ],
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // separate the youtube player so it can be reused
-                  YoutubePlayer(
-                    post: widget.post,
-                    autoPlay: widget.autoPlay,
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      '${widget.post.youtube['title']}',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      widget.post.title,
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      widget.post.content,
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        setState(() {});
-                        PostService.instance.showPostDetailScreen(
-                          context: context,
-                          post: widget.post,
-                        );
-                      },
-                      child: const Text('See Comments')),
-                  const Divider(),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text('You May Also Like'),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  PostListView(
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    category: 'youtube',
-                    separatorBuilder: (context, index) =>
-                        const SizedBox.shrink(),
-                    itemBuilder: (post, index) {
-                      if (post.id == widget.post.id) {
-                        return const SizedBox.shrink();
-                      }
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 8,
-                        ),
-                        child: GestureDetector(
-                            onTap: () {
-                              Navigator.replace(
-                                context,
-                                oldRoute: ModalRoute.of(context)!,
-                                newRoute: MaterialPageRoute(
-                                  builder: (context) => YoutubeScreen(
-                                    post: post,
-                                    autoPlay: widget.autoPlay,
-                                  ),
-                                ),
-                              );
-                            },
-                            child: YoutubeTile(post: post)),
-                      );
-                    },
-                  ),
-                ],
+                  child: Text('See Comments'.t)),
+              const Divider(),
+              const SizedBox(
+                height: 16,
               ),
-            )));
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text('You May Also Like'.t),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              PostListView(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                category: 'youtube',
+                separatorBuilder: (context, index) => const SizedBox.shrink(),
+                itemBuilder: (post, index) {
+                  if (post.id == widget.post.id) {
+                    return const SizedBox.shrink();
+                  }
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                    child: GestureDetector(
+                        onTap: () {
+                          Navigator.replace(
+                            context,
+                            oldRoute: ModalRoute.of(context)!,
+                            newRoute: MaterialPageRoute(
+                              builder: (context) => YoutubeScreen(
+                                post: post,
+                                autoPlay: widget.autoPlay,
+                              ),
+                            ),
+                          );
+                        },
+                        child: YoutubeTile(post: post)),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
