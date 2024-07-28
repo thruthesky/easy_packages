2666d75bd4476711325bedec7dcaaee1656c5dc9
JaeHo Song <thruthesky@gmail.com>
2024-07-29 00:37:56 +0800

    task crud

--- easy_task/lib/src/task.dart
+++ easy_task/lib/src/task.dart
@@ -274,16 +274,69 @@ class TaskUpdateScreen extends StatefulWidget {
 }
 
 class _TaskUpdateScreenState extends State<TaskUpdateScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _titleController;
+  late TextEditingController _descriptionController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.task.title);
+    _descriptionController =
+        TextEditingController(text: widget.task.description);
+  }
+
+  Future<void> _updateTask() async {
+    if (_formKey.currentState!.validate()) {
+      await widget.task.update(
+        title: _titleController.text,
+        description: _descriptionController.text,
+      );
+      Navigator.of(context).pop();
+      // TaskService.instance.showTaskDetailScreen(context, widget.task);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('TaskUpdate'),
+        title: const Text('Update Task'),
       ),
-      body: const Column(
-        children: [
-          Text("TaskUpdate"),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(labelText: 'Title'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a title';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(labelText: 'Description'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a description';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _updateTask,
+                child: const Text('Update Task'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -302,7 +355,7 @@ class _TaskListScreenState extends State<TaskListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('TaskList'),
+        title: const Text('Task List'),
       ),
       body: FirestoreListView(
         query: Task.col
