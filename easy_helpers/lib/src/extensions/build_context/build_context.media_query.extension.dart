3961a1ae4ed809a65b0e4b950cedd132e6572a7c
withcenterdev5 <withcenter.dev5@gmail.com>
2024-08-07 18:00:28 +0800

    build context mediaQuery

--- easy_helpers/lib/src/extensions/build_context/build_context.media_query.extension.dart
+++ easy_helpers/lib/src/extensions/build_context/build_context.media_query.extension.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 extension EasyHelperMediaQueryExtension on BuildContext {
+  Size get mediaQuerySize => MediaQuery.sizeOf(this);
+
   /// Small screen means if the width is smaller than 600
   /// If the device is a mobile phone and it is rotated in landscape mode,
   /// Then, the width is the longest side.
   /// If the phone is in portrait mode, then the width is shortest side.
-  bool get isSmallScreen => (MediaQuery.of(this).size.width < 600);
-  bool get isBigWidgetScreen => !isSmallScreen;
+  bool get isSmallScreen => (mediaQuerySize.width < 400);
+  bool get isMediumScreen => (mediaQuerySize.width < 800);
+  bool get isBigScreen => !isSmallScreen && !isMediumScreen;
 }
