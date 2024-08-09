833cad7534f5bda9e0e03d31feb1ade211556e60
JaeHo Song <thruthesky@gmail.com>
2024-08-09 23:36:27 +0800

    locale updates

--- easy_task/lib/src/task.locale.dart
+++ easy_task/lib/src/task.locale.dart
@@ -89,6 +89,7 @@ void addTaskLocaleTexts() async {
   if (locale == null) return;
 
   for (var entry in localeTexts.entries) {
+    if (lo.get(key: entry.key, locale: locale) != null) continue;
     lo.set(key: entry.key, locale: locale, value: entry.value[locale]);
   }
   TaskService.instance.setLocaleTexts?.call();
