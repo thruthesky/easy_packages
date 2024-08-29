efca63bb745841f6c07425e9dc959c4efaf2119b
JaeHo Song <thruthesky@gmail.com>
2024-08-29 13:52:09 +0800

    add user data

--- easychat/lib/src/widgets/chat.invitation_count.dart
+++ easychat/lib/src/widgets/chat.invitation_count.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:easychat/easychat.dart';
 import 'package:easyuser/easyuser.dart';
 import 'package:flutter/material.dart';
 
@@ -10,26 +8,10 @@ class ChatInvitationCount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: null,
-      builder: (context, snapshot) {
-        return FutureBuilder(
-            future: ChatService.instance.roomCol
-                .where(ChatRoom.field.invitedUsers, arrayContains: myUid)
-                .count()
-                .get(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              }
-              if (snapshot.hasError) {
-                return const Text('Error');
-              }
-              final countSnapshot = snapshot.data as AggregateQuerySnapshot;
-
-              return builder(countSnapshot.count ?? 0);
-            });
-      },
-    );
+    return MyDoc(builder: (user) {
+      return Text(
+        'No of chat invitation: ${user?.data?['chatInvitationCount'] ?? '0'}',
+      );
+    });
   }
 }
