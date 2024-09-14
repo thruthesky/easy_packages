c8eeb772b42f517f983f3f42eb695e0bc670307b
JaeHo Song <thruthesky@gmail.com>
2024-09-14 11:26:42 +0800

    update pubget

--- pubget.sh
+++ pubget.sh
@@ -11,6 +11,14 @@ for folder in "${folders[@]}"; do
   echo "Entering folder: $folder"
   cd "$folder" || continue  # 폴더로 이동, 실패 시 다음 폴더로 넘어감
   flutter pub get  # 현재 폴더의 내용 출력
+
+  # example 폴더가 있는지 검사
+  if [ -d "example" ]; then
+    echo "Entering example folder in $folder"
+    cd example
+    flutter pub get  # example 폴더에서 pub get 실행
+    cd ..  # example 폴더에서 나옴
+  fi
+
   cd ..  # 원래 폴더로 돌아옴
 done
-
