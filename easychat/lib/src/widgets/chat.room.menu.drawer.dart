ec8cdc0ea73759219dec725d7d0a8215b69f04ed
Christian Bucal (Dev2) <withcenter.dev2@gmail.com>
2024-09-16 09:29:18 +0800

    fix error when user has no display name

--- easychat/lib/src/widgets/chat.room.menu.drawer.dart
+++ easychat/lib/src/widgets/chat.room.menu.drawer.dart
@@ -20,8 +20,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: child,
       );
-  double photoHeight(BuildContext context) =>
-      200 + MediaQuery.of(context).padding.top;
+  double photoHeight(BuildContext context) => 200 + MediaQuery.of(context).padding.top;
 
   Widget label({required BuildContext context, required String text}) => Row(
         children: [
@@ -66,9 +65,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                               child: Icon(
                                 Icons.people,
                                 size: 64,
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onTertiaryContainer,
+                                color: Theme.of(context).colorScheme.onTertiaryContainer,
                               ),
                             ),
                       if (room!.masterUsers.contains(myUid))
@@ -78,10 +75,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                           child: IconButton(
                             icon: Container(
                               decoration: BoxDecoration(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .surface
-                                    .withAlpha(220),
+                                color: Theme.of(context).colorScheme.surface.withAlpha(220),
                                 borderRadius: BorderRadius.circular(40),
                               ),
                               padding: const EdgeInsets.all(8),
@@ -123,8 +117,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                       const SizedBox(height: 24),
                       label(
                         context: context,
-                        text: "members counted"
-                            .tr(args: {'num': room!.userUids.length}),
+                        text: "members counted".tr(args: {'num': room!.userUids.length}),
                       ),
                       const SizedBox(height: 8),
                     ],
@@ -149,8 +142,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                             ),
                     );
                   },
-                  itemCount:
-                      room!.userUids.length >= 4 ? 3 : room!.userUids.length,
+                  itemCount: room!.userUids.length >= 4 ? 3 : room!.userUids.length,
                 ),
                 if (room!.userUids.length >= 4) ...[
                   InkWell(
@@ -185,8 +177,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                   ListTile(
                     title: Text('invite more users'.t),
                     onTap: () async {
-                      final selectedUser =
-                          await UserService.instance.showSearchDialog(
+                      final selectedUser = await UserService.instance.showSearchDialog(
                         context,
                         itemBuilder: (user, index) {
                           return UserListTile(
@@ -209,8 +200,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                       if (room!.blockedUids.contains(selectedUser.uid)) {
                         throw ChatException(
                           'chat-blocked',
-                          'the user is blocked from the chat room and cannot invite'
-                              .t,
+                          'the user is blocked from the chat room and cannot invite'.t,
                         );
                       }
                       if (selectedUser.uid == my.uid) {
@@ -221,10 +211,8 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                       }
 
                       // Get if user is already invited
-                      final invitation = await ChatService.instance
-                          .invitedUserRef(selectedUser.uid)
-                          .child(room!.id)
-                          .get();
+                      final invitation =
+                          await ChatService.instance.invitedUserRef(selectedUser.uid).child(room!.id).get();
                       if (invitation.exists) {
                         dog("The user is already invited: ${invitation.value}");
                         throw ChatException(
@@ -276,8 +264,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                         const SizedBox(height: 24),
                         label(
                           context: context,
-                          text: "blocked users counted"
-                              .tr(args: {'num': room!.blockedUsers.length}),
+                          text: "blocked users counted".tr(args: {'num': room!.blockedUsers.length}),
                         ),
                         const SizedBox(height: 8),
                       ],
@@ -302,9 +289,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                               ),
                       );
                     },
-                    itemCount: room!.blockedUsers.length >= 4
-                        ? 3
-                        : room!.blockedUsers.length,
+                    itemCount: room!.blockedUsers.length >= 4 ? 3 : room!.blockedUsers.length,
                     // itemCount: room!.blockedUsers.length,
                   ),
                   if (room!.blockedUsers.length >= 4) ...[
@@ -323,9 +308,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: Theme.of(context).colorScheme.primaryContainer,
                   ),
-                  child: user != null &&
-                          user!.photoUrl != null &&
-                          user!.photoUrl!.isNotEmpty
+                  child: user != null && user!.photoUrl != null && user!.photoUrl!.isNotEmpty
                       ? CachedNetworkImage(
                           imageUrl: user!.photoUrl!,
                           fit: BoxFit.cover,
@@ -337,14 +320,13 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                 const SizedBox(height: 24),
                 horizontalPadding(
                   child: Text(
-                    user!.displayName,
+                    user?.displayName ?? "no name".t,
                     style: Theme.of(context).textTheme.titleLarge,
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                   ),
                 ),
-                if (user!.stateMessage != null &&
-                    user!.stateMessage!.isNotEmpty) ...[
+                if (user?.stateMessage != null && user!.stateMessage!.isNotEmpty) ...[
                   const SizedBox(height: 8),
                   horizontalPadding(
                     child: Text(
@@ -357,16 +339,14 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                 ],
               ],
               const SizedBox(height: 24),
-              if (user?.admin != true)
-                label(context: context, text: "options".t),
+              if (user?.admin != true) label(context: context, text: "options".t),
               const SizedBox(height: 8),
               if (room?.joined == true) ...[
                 if (room!.group && room!.masterUsers.contains(my.uid))
                   ListTile(
                     title: Text("update".t),
                     onTap: () {
-                      ChatService.instance
-                          .showChatRoomEditScreen(context, room: room);
+                      ChatService.instance.showChatRoomEditScreen(context, room: room);
                     },
                   ),
                 // if (room!.group)
@@ -393,8 +373,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
                 if (room?.single == true || user != null)
                   ListTile(
                     title: Text(
-                      UserService.instance.blockChanges.value
-                              .containsKey(user!.uid)
+                      UserService.instance.blockChanges.value.containsKey(user!.uid)
                           ? "unblock".t
                           : "block".t,
                     ),
@@ -448,8 +427,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
     showDialog(
       context: context,
       builder: (context) {
-        return ChatService.instance.membersDialogBuilder
-                ?.call(context, room!) ??
+        return ChatService.instance.membersDialogBuilder?.call(context, room!) ??
             ChatRoomMemberListDialog(room: room!);
       },
     );
@@ -459,8 +437,7 @@ class ChatRoomMenuDrawer extends StatelessWidget {
     showDialog(
       context: context,
       builder: (context) {
-        return ChatService.instance.blockedUsersDialogBuilder
-                ?.call(context, room!) ??
+        return ChatService.instance.blockedUsersDialogBuilder?.call(context, room!) ??
             ChatRoomBlockedUsersDialog(room: room!);
       },
     );
