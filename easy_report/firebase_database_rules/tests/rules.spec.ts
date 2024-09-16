41e0142dff68a2d5db824c8f1255c2d842e7a7e4
JaeHo Song <thruthesky@gmail.com>
2024-09-16 20:48:55 +0800

    fix rules

--- easy_report/firebase_database_rules/tests/rules.spec.ts
+++ easy_report/firebase_database_rules/tests/rules.spec.ts
@@ -5,59 +5,92 @@ import {
     initializeTestEnvironment
 } from "@firebase/rules-unit-testing";
 import firebase from 'firebase/compat/app';
-import { ref, get, update } from 'firebase/database';
+import { ref, get, update, orderByChild, equalTo, query } from 'firebase/database';
 import { readFileSync } from "node:fs";
 import { before } from "mocha";
 
 
+
 /****** SETUP ********/
 const PROJECT_ID = 'fakeproject2';
-const host = '127.0.0.1'; // localhost 로 하면 안된다.
-// 터미널에 표시되는 포트로 적절히 지정해야 한다. Emulators 를 실행하면, 터미널에 아래와 같이 포트가 DB 포트가 표시된다.
-// 예) Database │ 127.0.0.1: 9000
+const host = '127.0.0.1'; // Do not use localhost.
+// Set this to the appropriate port displayed in the terminal when you run the Emulators.
+// Example: Database │ 127.0.0.1: 9000
 const port = 9000;
 let testEnv: RulesTestEnvironment;
 
 
 
-describe('전반적인 규칙 테스트', async () => {
-    // 모든 테스트를 시작하기 전에 실행되는 콜백 함수.
-    // 여기에 initializeTestEnvironment() 를 호출해서, 테스트 환경을 초기화 하면 된다.
-    // Firestore, Realtime Database, Storage 등을 모두 같이 쓰는 코드이다.
+describe('Overall Rules Test', async () => {
+    // Initialize the test environment once on every run.
     before(async () => {
-        /// 모든 테스트를 실행하기 전에, 테스트 환경 초기화.
+        /// Initialize the test environment before running all tests.
         testEnv = await initializeTestEnvironment({
             projectId: PROJECT_ID,
             database: {
                 host,
                 port,
-                // Firestore Security Rules 파일을 읽어서, 테스트 환경에 적용한다.
-                // 즉, Security Rules 파일을 수정하고, 테스트를 다시 실행하면, 수정된 Rules 이 적용되므로,
-                // mocha watch 를 하는 경우, 소스 코드를 수정 필요 없이 저장만 한번 해 주면 된다.
-                rules: readFileSync('database.rules.json', 'utf8')
+                // Read the Database Security Rules file once every run.
+                rules: JSON.stringify({
+                    "rules": {
+                        "users": {
+                            ".read": true,
+                            "$uid": {
+                                ".write": "$uid === auth.uid"
+                            },
+                            ".indexOn": [
+                                "createdAt"
+                            ]
+                        },
+                        "reports": {
+                            "$reportKey": {
+                                ".read": "data.child('reporter').val() === auth.uid",
+                                ".write": true
+                            },
+                            ".indexOn": [
+                                "reporter",
+                                "reportee",
+                                "path"
+                            ]
+                        }
+                    }
+                }),
             },
         });
 
     });
 
-    // 각 테스트를 하기 전에, 로컬 Firestore 의 데이터를 모두 지운다.
+    // Clear all data in the local Firestore before each test.
     beforeEach(async () => {
         await testEnv.clearDatabase();
 
-        // 셋업: Security Rules 를 적용하지 않고, 테스트 데이터를 미리 좀 저장해 놓는다.
-        // withSecurityRulesDisabled 는 한번에 하나의 쿼리만 실행해야 한다. 그렇지 않으면,
-        // xxxx has already been started and its settings can no longer be change 에러가 발생한다.
+        // Setup: some sample data by passing the security rules
         await testEnv.withSecurityRulesDisabled(async (context) => {
             await context.database().ref('test/a').set({ name: 'apple', no: 1 });
         });
 
     });
 
-    it('공개 프로필 읽기 테스트 - 성공 - 로그인 하지 않고 참조', async () => {
+    it('Public Profile Read Test - Success - Reference without logging in', async () => {
         // Setup: Create Rules Test Context
         const unauthedDb = testEnv.unauthenticatedContext().database();
-        // const snapshot = await get(ref(unauthedDb, 'users/foobar'));
+        await assertSucceeds(get(ref(unauthedDb, 'users/foobar')));
+    });
 
-        await assertFails(get(ref(unauthedDb, 'users/foobar')));
+    it('Get my report', async () => {
+        await testEnv.withSecurityRulesDisabled(async (context) => {
+            await context.database().ref('reports/report-1').set({ reporter: 'user1' });
+        });
+        const authedDb = testEnv.authenticatedContext('user1').database();
+        await assertSucceeds(get(ref(authedDb, 'reports/report-1')));
+    });
+
+    it('Get the list of my reports', async () => {
+        await testEnv.withSecurityRulesDisabled(async (context) => {
+            await context.database().ref('reports/report-1').set({ reporter: 'user1' });
+        });
+        const authedDb = testEnv.authenticatedContext('user1').database();
+        const reportsQuery = query(ref(authedDb, 'reports'), orderByChild('reporter'), equalTo('user1'));
+        await assertSucceeds(get(reportsQuery));
     });
 });
