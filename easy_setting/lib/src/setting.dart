9691fd1b030f214aee8781722dd95b968e0277d5
JaeHo Song <thruthesky@gmail.com>
2024-09-20 12:15:10 +0800

    error: removing firestore

--- easy_setting/lib/src/setting.dart
+++ easy_setting/lib/src/setting.dart
@@ -1,20 +1,24 @@
-import 'package:easy_firestore/easy_firestore.dart';
+import 'package:easy_realtime_database/easy_realtime_database.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 /// TODO: move it to realtime database
 class Setting extends StatelessWidget {
-  const Setting({super.key, required this.id, required this.builder});
+  const Setting({
+    super.key,
+    required this.id,
+    required this.builder,
+  });
 
   final String id;
-  final Widget Function(DocumentModel) builder;
+  final Widget Function(dynamic value, DatabaseReference ref) builder;
 
   @override
   Widget build(BuildContext context) {
-    return Document(
-      collectionName: 'settings',
-      id: id,
-      builder: (model) {
-        return builder(model);
+    return Value(
+      ref: FirebaseDatabase.instance.ref().child('settings').child(id),
+      builder: (v, r) {
+        return builder(v, r);
       },
     );
   }
