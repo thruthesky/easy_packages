6a3fe25e6a22a0470310089c2283da7d75438037
withcenterdev5 <withcenter.dev5@gmail.com>
2024-09-23 08:55:23 +0800

    example in easy_post

--- easy_post/example/lib/main.dart
+++ easy_post/example/lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:example/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:easyuser/easyuser.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,18 +30,21 @@ class MyApp extends StatelessWidget {
                 child: EmailPasswordLogin(),
               )
             : SingleChildScrollView(
-                child: Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        UserService.instance.signOut();
-                      },
-                      child: const Text('Sign Out'),
-                    ),
-                    Text(user.uid),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          UserService.instance.signOut();
+                        },
+                        child: const Text('Sign Out'),
+                      ),
+                      Text(user.uid),
 
-                    // ElevatedButton(onPressed: onPressed, child: Text('Create Post')),
-                  ],
+                      // ElevatedButton(onPressed: onPressed, child: Text('Create Post')),
+                    ],
+                  ),
                 ),
               ),
       ),
