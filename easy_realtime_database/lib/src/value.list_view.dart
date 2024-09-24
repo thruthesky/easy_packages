e86b2e60923c01174b37f1454a380ba9fa3a978b
JaeHo Song <thruthesky@gmail.com>
2024-09-24 18:19:31 +0800

    adding value list view

---
+++ easy_realtime_database/lib/src/value.list_view.dart
@@ -0,0 +1,119 @@
+import 'dart:developer';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_ui_database/firebase_ui_database.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+/// PostListView
+///
+///
+class PostListView extends StatelessWidget {
+  const PostListView({
+    super.key,
+    required this.query,
+    this.pageSize = 20,
+    this.loadingBuilder,
+    this.errorBuilder,
+    this.separatorBuilder,
+    this.scrollDirection = Axis.vertical,
+    this.reverse = false,
+    this.controller,
+    this.primary,
+    this.physics,
+    this.shrinkWrap = false,
+    this.padding,
+    this.addAutomaticKeepAlives = true,
+    this.addRepaintBoundaries = true,
+    this.addSemanticIndexes = true,
+    this.cacheExtent,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.onDrag,
+    this.restorationId,
+    this.clipBehavior = Clip.hardEdge,
+    this.itemBuilder,
+    this.emptyBuilder,
+  });
+  final Query query;
+
+  final int pageSize;
+  final Widget Function()? loadingBuilder;
+  final Widget Function(String)? errorBuilder;
+  final Widget Function(BuildContext, int)? separatorBuilder;
+  final Axis scrollDirection;
+  final bool reverse;
+  final ScrollController? controller;
+  final bool? primary;
+  final ScrollPhysics? physics;
+  final bool shrinkWrap;
+  final EdgeInsetsGeometry? padding;
+  final bool addAutomaticKeepAlives;
+  final bool addRepaintBoundaries;
+  final bool addSemanticIndexes;
+  final double? cacheExtent;
+  final DragStartBehavior dragStartBehavior;
+  final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
+  final String? restorationId;
+  final Clip clipBehavior;
+  final Widget Function(dynamic, int)? itemBuilder;
+  final Widget Function()? emptyBuilder;
+  @override
+  Widget build(BuildContext context) {
+    return FirebaseDatabaseQueryBuilder(
+      query: query,
+      pageSize: pageSize,
+      builder: (context, snapshot, _) {
+        if (snapshot.isFetching) {
+          return loadingBuilder?.call() ?? const Center(child: CircularProgressIndicator.adaptive());
+        }
+
+        if (snapshot.hasError) {
+          log('Error: ${snapshot.error}');
+          return errorBuilder?.call(snapshot.error.toString()) ??
+              Text('Something went wrong! ${snapshot.error}');
+        }
+
+        if (snapshot.hasData && snapshot.docs.isEmpty && !snapshot.hasMore) {
+          return emptyBuilder?.call() ?? const Center(child: Text('The node is empty'));
+        }
+
+        return ListView.separated(
+          itemCount: snapshot.docs.length,
+          separatorBuilder: (context, index) =>
+              separatorBuilder?.call(context, index) ?? const SizedBox.shrink(),
+          scrollDirection: scrollDirection,
+          reverse: reverse,
+          controller: controller,
+          primary: primary,
+          physics: physics,
+          shrinkWrap: shrinkWrap,
+          padding: padding,
+          addAutomaticKeepAlives: addAutomaticKeepAlives,
+          addRepaintBoundaries: addRepaintBoundaries,
+          addSemanticIndexes: addSemanticIndexes,
+          cacheExtent: cacheExtent,
+          dragStartBehavior: dragStartBehavior,
+          keyboardDismissBehavior: keyboardDismissBehavior,
+          restorationId: restorationId,
+          clipBehavior: clipBehavior,
+          itemBuilder: (context, index) {
+            // if we reached the end of the currently obtained items, we try to
+            // obtain more items
+            if (snapshot.hasMore && index + 1 == snapshot.docs.length) {
+              // Tell FirebaseDatabaseQueryBuilder to try to obtain more items.
+              // It is safe to call this  function from within the build method.
+              snapshot.fetchMore();
+            }
+
+            final v = snapshot.docs[index];
+
+            return itemBuilder?.call(v, index) ??
+                ListTile(
+                  title: Text('$index: ${snapshot.docs[index].key}'),
+                );
+          },
+        );
+      },
+    );
+  }
+}
